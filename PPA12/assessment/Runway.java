2095c3942f120e1716a8418afc2a8b191b8c3baf
Samuel J. Godwin <55901161+SamuelGodwin@users.noreply.github.com>
2021-01-20 19:15:57 +0000

    Add files via upload

---
+++ PPA12/assessment/Runway.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package assessment;
+
+/**
+ * @author Samuel & Anttoni
+ * 
+ * This class models the runway in our solution. It has 2 fields; length and width.
+ * It contains two accessor methods.
+ * This class and its methods are public, so are accessible anywhere.
+ *
+ */
+
+public class Runway {
+	private final int length;
+	private final int width;
+	
+	/**
+	 * 
+	 * @param length
+	 * @param width
+	 */
+	public Runway(int length, int width) {
+
+		this.length = length; // length of runway
+		this.width = width; // width of runway
+		
+	}
+	
+	/**
+	 * 'getLength'. This is an accessor method for the length field.
+	 * @return length
+	 */
+	public int getLength() {
+		
+		return length;
+		
+	}
+	/**
+	 * 'getWidth'. This is an accessor method for the width field.
+	 * @return width
+	 */
+	public int getWidth() {
+		
+		return width;
+		
+	}
+}
